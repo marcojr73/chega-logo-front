649684f9921b0ea357eff93811c673271e24a76b
marcojr73 <marcojuniorengineer@gmail.com>
2023-02-13 09:08:32 -0300

    fix: single provider main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,18 @@
 import React from "react"
 import type { AppProps } from "next/app"
-import { ThemeContextProvider } from "@/providers/ThemeProvider"
 import Theme from "./_theme"
 
 import "../styles/reset.css"
 import dynamic from "next/dynamic"
-import { TruckesContextProvider } from "@/providers/truckesProvider"
-import { PlacesContextProvider } from "@/providers/placesProvider"
 import "../config/i18n"
+import Providers from "@/providers"
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeContextProvider>
-      <TruckesContextProvider>
-        <PlacesContextProvider>
-
-          <Theme />
-          <Component {...pageProps} />
-          
-        </PlacesContextProvider>
-      </TruckesContextProvider>
-    </ThemeContextProvider>
+    <Providers>
+      <Theme />
+      <Component {...pageProps} />
+    </Providers>
   )
 }
 
